d95c732ada9f2b19208f652cb284c49169d02973
nurfandi31 <fandinur241@gmail.com>
2026-09-02 15:47:51 +0700

    kop surat

--- resources/views/laporan-keuangan/layout/kop.blade.php
+++ resources/views/laporan-keuangan/layout/kop.blade.php
@@ -2,19 +2,19 @@
     <tr>
         <td style="width:110px; padding:0 12px 0 0; vertical-align:middle; text-align:center;">
             @if (!empty($logo))
-                <img src="data:image/{{ $logo_type }};base64,{{ $logo }}" height="80"
+                <img src="data:image/{{ $logo_type }};base64,{{ $logo }}" height="60"
                     style="display:block; margin:0 auto;">
             @endif
         </td>
         <td style="padding:0; vertical-align:middle; text-align:left;">
-            <div style="font-weight:bold; font-size:18px; line-height:1.15; margin:0;">
+            <div style="font-weight:bold; font-size:14px; line-height:1.15; margin:0;">
                 {{ strtoupper($profil->nama ?? 'SISTEM AKADEMIK') }}
             </div>
-            <div style="font-size:10px; color:#555; line-height:1.15; margin:2px 0 0 0;">
+            <div style="font-size:9px; color:#555; line-height:1.15; margin:2px 0 0 0;">
                 <i>{{ $profil->alamat ?? '-' }}</i>
             </div>
             @if (!empty($profil->telpon) || !empty($profil->email))
-                <div style="font-size:10px; color:#555; line-height:1.15; margin:2px 0 0 0;">
+                <div style="font-size:9px; color:#555; line-height:1.15; margin:2px 0 0 0;">
                     @if (!empty($profil->telpon))Telp. {{ $profil->telpon }}@endif
                     @if (!empty($profil->telpon) && !empty($profil->email)) &middot; @endif
                     @if (!empty($profil->email))Email: {{ $profil->email }}@endif
